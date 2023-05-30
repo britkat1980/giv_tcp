ff870987cdeaac231233ddab0540314f28b068a9
salewis38 <46199139+salewis38@users.noreply.github.com>
2023-05-30 22:11:23 +0100

    Update palm_soc.py
    Re-aligned with current palm.py v0.8.6:
    * Simplified solcast weighting format
    * Expanded for >4 hours off-peak time
    * Various housekeeping edits
    
    Requires updates palm_settings.py
    NB: these changes are currently UNTESTED in HomeAssistant

--- GivTCP/palm_soc.py
+++ GivTCP/palm_soc.py
@@ -7,15 +7,15 @@ import json
 from datetime import datetime, timedelta
 from typing import Tuple
 import requests
-import palm_settings as stgs
-from settings import GiV_Settings
 from GivLUT import GivLUT
 import write as wr
+import palm_settings as stgs
+from settings import GiV_Settings
 
 
 # This software in any form is covered by the following Open Source BSD license:
 #
-# Copyright 2022, Steve Lewis
+# Copyright 2023, Steve Lewis
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification, are permitted
@@ -46,8 +46,12 @@ import write as wr
 
 # v0.8.3aSoC   28/Jul/22 Branch from palm - SoC only
 # v0.8.3bSoC   05/Aug/22 Improved commenting, removed legacy code
+# v0.8.6SoC    30/May/23 Re-aligned with palm.py v0.8.6:
+#                          Simplified solcast weighting format
+#                          Expanded for >4 hours off-peak time
+#                          Various housekeeping edits
 
-PALM_VERSION = "v0.8.3bSoC"
+PALM_VERSION = "v0.8.6bSoC"
 
 #  Future improvements:
 #
@@ -94,7 +98,7 @@ class GivEnergyObj:
             while index < 284:
                 try:
                     current_energy = float(history['data'][index]['today']['consumption'])
-                except:
+                except Exception:
                     break
                 if counter == 0:
                     self.base_load[counter] = round(current_energy, 1)
@@ -105,127 +109,119 @@ class GivEnergyObj:
                 index += 12
             logger.info("Load Calc Summary: "+ str(current_energy) + " " + str(self.base_load))
 
-    def set_inverter_register(self, register: str, value: str):
-        """Set target charge for overnight charging."""
-
-        url = stgs.GE.url + "settings/" + register + "/write"
-        key = stgs.GE.key
-        headers = {
-            'Authorization': 'Bearer  ' + key,
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-
-        payload = {
-            'value': value
-        }
-
-        resp = ""
-        if not DEBUG_MODE:
-            try:
-                resp = requests.request('POST', url, headers=headers, json=payload)
-            except requests.exceptions.RequestException as error:
-                logger.error(error)
-                return
-
-        if register == "64":
-            reg_str = "(AC Charge 1 Start Time)"
-        elif register == "71":
-            reg_str = "(Battery Reserve)"
-        elif register == "77":
-            reg_str = "(AC Charge Upper % Limit)"
-        else:
-            reg_str = "(Unknown)"
-
-        logger.info("Setting "+ str(reg_str)+ " to "+ str(value)+ "%")
-
     def set_soc(self, tgt_soc):
         """ Sets start time and target SoC for overnight charge."""
-#        self.set_inverter_register("77", tgt_soc)
         result={}
         logger.debug("Setting Charge Target to: "+ str(tgt_soc)+ "%")
         payload={}
         payload['chargeToPercent']= tgt_soc
         result=wr.setChargeTarget(payload)
         logger.debug(result)
-        
-    def restore_params(self):
-        """Restore inverter parameters after overnight charge"""
 
-        self.set_inverter_register("77", 100)
-        self.set_inverter_register("64", stgs.GE.start_time)
-        self.set_inverter_register("71", stgs.GE.batt_reserve)
-
-
-    def compute_tgt_soc(self, gen_fcast, wgt_10: int, wgt_50: int, wgt_90: int, month: str, commit: bool):
+    def compute_tgt_soc(self, gen_fcast, weight: int, commit: bool):
         """Compute tomorrow's overnight SoC target"""
 
         batt_max_charge: float = stgs.GE.batt_max_charge
 
         tgt_soc = 100
-        if gen_fcast.pv_est50_day[0] > 0:
-            if month in stgs.GE.winter:
-                logger.info("Info; winter month, SoC set to 100%")
+
+        if MONTH_VAR in stgs.GE.winter:
+            logger.info("Info; winter month, SoC set to 100%")
+            self.set_soc(tgt_soc)
+            return
+
+        # Solcast provides 3 estimates (P10, P50 and P90). Compute individual weighting
+        # factors for each of the 3 estimates from the weight input parameter, using a
+        # triangular approximation for simplicity
+
+        weight = min(max(weight,10),90)  # Range check
+        wgt_10 = max(0, 50 - weight)
+        if weight > 50:
+            wgt_50 = 90 - weight
+        else:
+            wgt_50 = weight - 10
+        wgt_90 = max(0, weight - 50)
+
+        if gen_fcast.pv_est50_day[0] > 0:  # Quick check for valid generation data
+
+            # The clever bit:
+            # Start with a battery at 100%. For each hour of the coming day, calculate the
+            # battery charge based on forecast generation and historical usage. Capture values for
+            # maximum charge and also the minimum charge value at any time before the maximum.
+
+            batt_max_charge: float = stgs.GE.batt_max_charge
+            batt_charge: float = [0] * 24
+            batt_charge[0] = batt_max_charge
+            max_charge = 0
+            min_charge = batt_max_charge
+
+            logger.info("")
+            logger.info("{:>10} {:>10} {:>10}  {:>10} {:>10}".format(
+                "Hour", "Charge", "Cons", "Gen", "SoC"))
+
+            if stgs.GE.end_time != "":
+                end_charge_period = int(stgs.GE.end_time[0:2])
             else:
+                end_charge_period = 4
 
-                #  Step through forecast generation and consumption for coming day to identify
-                #  lowest minimum before overcharge and maximum overcharge. If there is overcharge
-                #  and the first min is above zero, reduce overnight charge for min export.
-
-                batt_charge: float = [0] * 24
-                batt_charge[0] = batt_max_charge
-                max_charge = 0
-                min_charge = batt_max_charge
-
-                logger.info("")
-                logger.info("{:>10} {:>10} {:>10}  {:>10} {:>10}".format(
-                    "Hour", "Charge", "Cons", "Gen", "SoC"))
-
-                index = 0
-                while index < 24:
-                    if index > 4:  # Battery is in Eco mode
-                        total_load = ge.base_load[index]
-                    else:  # Battery is in Charge mode
-                        total_load = 0
-                    est_gen = (gen_fcast.pv_est10_hrly[index] * wgt_10 +
-                        gen_fcast.pv_est50_hrly[index] * wgt_50 +
-                        gen_fcast.pv_est90_hrly[index] * wgt_90) / (wgt_10 + wgt_50 + wgt_90)
-                    if index > 0:
-                        batt_charge[index] = (batt_charge[index - 1] +
-                            max(-1 * stgs.GE.charge_rate,
-                                min(stgs.GE.charge_rate, est_gen - total_load)))
-                        # Capture min charge on lowest down-slope before charge exceeds 100%
-                        if (batt_charge[index] <= batt_charge[index - 1] and
-                            max_charge < batt_max_charge):
-                            min_charge = min(min_charge, batt_charge[index])
-                        elif index > 4:  # Charging after overnight boost
-                            max_charge = max(max_charge, batt_charge[index])
-
-                    logger.info("{:>10} {:>10} {:>10}  {:>10} {:>10}".format(
-                        index, round(batt_charge[index], 2), round(total_load, 2),
-                        round(est_gen, 2), int(100 * batt_charge[index] / batt_max_charge)))
-
-                    index += 1
-
-                max_charge_pcnt = int(100 * max_charge / batt_max_charge)
-                min_charge_pcnt = int(100 * min_charge / batt_max_charge)
-
-                #  Reduce nightly charge to capture max export
-                if month in stgs.GE.shoulder:
-                    tgt_soc = max(stgs.GE.max_soc_target, 100 - min_charge_pcnt, 200 - max_charge_pcnt)
+            index = 0
+            est_gen = 0
+            while index < 24:
+                if index <= end_charge_period:  # Battery is in AC Charge mode
+                    total_load = 0
                 else:
-                    tgt_soc = max(stgs.GE.min_soc_target, 100 - min_charge_pcnt, 200 - max_charge_pcnt)
-                tgt_soc = int(min(max(tgt_soc, 0), 100))  # Limit range
+                    total_load = ge.base_load[index]
 
-                logger.info("")
-                logger.info("{:<25} {:>10} {:>10} {:>10} {:>10} {:>10}".format("SoC Calc Summary;",
-                    "Max Charge", "Min Charge", "Max %", "Min %", "Target SoC"))
-                logger.info("{:<25} {:>10} {:>10} {:>10} {:>10} {:>10}".format("SoC Calc Summary;",
-                    round(max_charge, 2), round(min_charge, 2),
-                    max_charge_pcnt, min_charge_pcnt, tgt_soc))
-                logger.info("{:<25} {:>10} {:>10} {:>10} {:>10} {:>10}".format("SoC (Adjusted);",
-                    round(max_charge, 2), round(min_charge, 2),
-                    max_charge_pcnt - 100 + tgt_soc, min_charge_pcnt - 100 + tgt_soc, "\n"))
+                if index > 0:
+                    # Generation is in GMT, add an hour for BST
+                    est_gen = (gen_fcast.pv_est10_hrly[index - 1] * wgt_10 +
+                        gen_fcast.pv_est50_hrly[index - 1] * wgt_50 +
+                        gen_fcast.pv_est90_hrly[index - 1] * wgt_90) / (wgt_10 + wgt_50 + wgt_90)
+
+                    batt_charge[index] = (batt_charge[index - 1] +
+                        max(-1 * stgs.GE.charge_rate,
+                            min(stgs.GE.charge_rate, est_gen - total_load)))
+                    # Capture min charge on lowest down-slope before charge exceeds 100% append
+                    # max xharge if on an up slope after overnight charge
+                    if (batt_charge[index] <= batt_charge[index - 1] and
+                        max_charge < batt_max_charge):
+                        min_charge = min(min_charge, batt_charge[index])
+                    elif index > end_charge_period:  # Charging after overnight boost
+                        max_charge = max(max_charge, batt_charge[index])
+
+                logger.info("{:>10} {:>10} {:>10}  {:>10} {:>10}".format(
+                    time_to_hrs(index * 60), round(batt_charge[index], 2), round(total_load, 2),
+                    round(est_gen, 2), int(100 * batt_charge[index] / batt_max_charge)))
+
+                index += 1
+
+            max_charge_pcnt = int(100 * max_charge / batt_max_charge)
+            min_charge_pcnt = int(100 * min_charge / batt_max_charge)
+
+            # low_soc is the minimum SoC target. Provide more buffer capacity in shoulder months
+            if MONTH_VAR in stgs.GE.shoulder:
+                low_soc = stgs.GE.max_soc_target
+            else:
+                low_soc = stgs.GE.min_soc_target
+
+            # The really clever bit is just two lines:
+            # Reduce the target SoC to the greater of:
+            #     The surplus above 100% for max_charge_pcnt
+            #     The spare capacity in the battery before the maximum charge point
+            #     The preset minimum value
+            # Range check the resulting value
+            tgt_soc = max(200 - max_charge_pcnt, 100 - min_charge_pcnt, low_soc)
+            tgt_soc = int(min(tgt_soc, 100))  # Limit range to 100%
+
+            logger.info("")
+            logger.info("{:<25} {:>10} {:>10} {:>10} {:>10} {:>10}".format("SoC Calc Summary;",
+                "Max Charge", "Min Charge", "Max %", "Min %", "Target SoC"))
+            logger.info("{:<25} {:>10} {:>10} {:>10} {:>10} {:>10}".format("SoC Calc Summary;",
+                round(max_charge, 2), round(min_charge, 2),
+                max_charge_pcnt, min_charge_pcnt, tgt_soc))
+            logger.info("{:<25} {:>10} {:>10} {:>10} {:>10} {:>10}".format("SoC (Adjusted);",
+                round(max_charge, 2), round(min_charge, 2),
+                max_charge_pcnt - 100 + tgt_soc, min_charge_pcnt - 100 + tgt_soc, "\n"))
 
         else:
             logger.error("Incomplete Solcast data, setting target SoC to 100%")
@@ -407,15 +403,15 @@ if __name__ == '__main__':
 
     # Compute & set SoC target
     logger.debug("10% forecast...")
-    ge.compute_tgt_soc(solcast, 1, 0, 0, MONTH_VAR, False)
+    ge.compute_tgt_soc(solcast, 10, True)
     logger.debug("50% forecast...")
-    ge.compute_tgt_soc(solcast, 0, 1, 0, MONTH_VAR, False)
+    ge.compute_tgt_soc(solcast, 50, True)
     logger.debug("90% forecast...")
-    ge.compute_tgt_soc(solcast, 0, 0, 1, MONTH_VAR, False)
-    
+    ge.compute_tgt_soc(solcast, 90, True)
+
     # Write final SoC target to GivEnergy register
-    # Change weighting in command according to desired risk/reward profile
-    logger.debug("1:2:0 weighted forecast...")
-    ge.compute_tgt_soc(solcast, 1, 2, 0, MONTH_VAR, True)
+    # Change weighting in settings file for desired risk/reward profile
+    logger.debug("Forecast weighting %: ", stgs.Solcast.weight)
+    ge.compute_tgt_soc(solcast, stgs.Solcast.weight, True)
 
 # End
